af6688e76d311fa5df385bccb0a9f2fb8ef37a36
Vikram Singh <vs125609@gmail.com>
2023-04-28 22:19:36 +0530

    poly fill for bind

--- polyfill-for-bind.js
+++ polyfill-for-bind.js
@@ -6,12 +6,12 @@ const name0 = {
     last_name: 'Singh'
 }
 
-const print_name = function () {
-    console.log(this.first_name + " " + this.last_name);
+const print_name = function (hometown, capitaltown, gaontown) {
+    console.log(this.first_name + " " + this.last_name + " from " + hometown + " to " + capitaltown + " go " + gaontown);
 }
 
-const print_name_bind0 = print_name.bind(name0);
-print_name_bind0()
+const print_name_bind0 = print_name.bind(name0, "Delhi", "Ayodhya");
+print_name_bind0("Mumbai")
 
 // Now what we want ? Let's clearify the things
 // we want the bind function should be define by ourself
@@ -25,12 +25,13 @@ print_name_bind0()
 
 // if we keep any methond in Function.prototype then each and every method what we write has access to those methods
 
-Function.prototype.my_bind = function (...args) {
+Function.prototype.my_bind = function (...args0) {
     let func = this;
-    return function () {
-        func.call(args[0]);
+    const params = args0.slice(1);
+    return function (...args1) {
+        func.apply(args0[0], [...params,...args1]);
     }
 }
 
-const print_name_my_bind = print_name.my_bind(name0);
-print_name_my_bind();
+const print_name_my_bind = print_name.my_bind(name0, "Delhi", "Mumbai");
+print_name_my_bind("Ayodhya");
